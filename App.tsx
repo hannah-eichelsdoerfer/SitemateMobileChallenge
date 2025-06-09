915be7e3db8006df23d0bf4d490bfc2d187a6f5f
Hannah Eichelsdoerfer <hannaheichelsdoerfer@sitemate.com>
2025-06-09 20:10:00 +1000

    Initial commit - clean boilerplate

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>React Native Boilerplate</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
